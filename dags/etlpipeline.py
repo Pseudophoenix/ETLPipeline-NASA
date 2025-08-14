df850051b88da9913baae3c1f640ddba3d6e81b7
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-15 01:28:47 +0530

    Resolved

--- dags/etlpipeline.py
+++ dags/etlpipeline.py
@@ -12,5 +12,36 @@ with DAG(
     catchup=False
 )as dag:
     # step 1 : Create the table if it does not exist
+    @task
+    def create_table():
+        ## initialize the PostgresHook
+        postgres_hook=PostgresHook(postgres_conn_id="my_postgres_connection")
 
-    # step 2: Extract the NASA API data(APOD)
+        ## SQL query to create the table 
+        create_table_query="""
+        CREATE TABLE IF NOT EXISTS apod_data (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255),
+            explaination TEXT,
+            url TEXT,
+            data DATE,
+            media_type VARCHAR(50)
+        )
+        """
+
+
+
+    # step 2: Extract the NASA API data(APOD)-Astronomy Picture of the Day(Extract pipeline)
+
+
+
+    # step 3: Transform the data(Pick the information that i need to save)
+
+
+    # step 4: Load the data into the Postgres SQL
+
+
+    # step 5: Verify the data DBViewer
+
+
+    # step 6: Define the task dependencies
